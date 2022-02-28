5d67f5f58383bd0fbcef325160f59e2027deca84
pskishere <pskishere@gmail.com>
2022-02-28 22:27:48 +0800

    add bmap

--- components/+maps2/layout.tsx
+++ components/+maps2/layout.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import { BaiduMap } from '../bmap';
+import { BMapMapvglLayer, BMapMapvglView } from '../bmap/vgl';
 export default class Layout extends React.Component {
 	componentDidMount() {}
 	render() {
-		return <BaiduMap />;
+		return (
+			<BaiduMap>
+				<BMapMapvglView effects={['bright']}>
+					<BMapMapvglLayer
+						type='PointLayer'
+						data={[]}
+						options={{
+							blend: 'lighter',
+							size: 12,
+							color: 'rgb(255, 53, 0, 0.6)',
+						}}
+					/>
+				</BMapMapvglView>
+			</BaiduMap>
+		);
 	}
 }
