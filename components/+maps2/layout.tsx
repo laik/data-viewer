64f7c42a774e45d86e9375d80e3ccc859a6e7c29
pskishere <pskishere@gmail.com>
2022-03-02 14:46:28 +0800

    add TrackAnimation

--- components/+maps2/layout.tsx
+++ components/+maps2/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BaiduMap } from '../bmap';
+import { BMapTrackAnimation } from '../bmap/animation';
 import { BMapPrism } from '../bmap/custom-overlay';
 import { withMapApi } from '../bmap/wrapper';
 import hp from './data/hp.json';
@@ -7,52 +8,59 @@ import lw from './data/lw.json';
 
 @withMapApi
 export default class Layout extends React.Component {
+	handleBMapPrismClick(e) {
+		console.log('---->e', e.target);
+	}
 
-    handleBMapPrismClick(e) {
-        console.log("---->e", e.target);
-    }
+	render() {
+		let lwpath = [];
+		for (let i = 0; i < lw.length; i++) {
+			const points = lw[i];
+			let lng = points[0];
+			let lat = points[1];
+			lwpath.push(new BMapGL.Point(Number(lng), Number(lat)));
+		}
+		let hppath = [];
+		for (let i = 0; i < hp.length; i++) {
+			const points = hp[i];
+			let lng = points[0];
+			let lat = points[1];
+			hppath.push(new BMapGL.Point(Number(lng), Number(lat)));
+		}
 
+		var pl = new BMapGL.Polyline(hppath);
+		// BMapTrackAnimation.start();
 
-    render() {
-        let lwpath = [];
-        for (let i = 0; i < lw.length; i++) {
-            const points = lw[i];
-            let lng = points[0];
-            let lat = points[1];
-            lwpath.push(new BMapGL.Point(Number(lng), Number(lat)));
-        }
-        let hppath = [];
-        for (let i = 0; i < hp.length; i++) {
-            const points = hp[i];
-            let lng = points[0];
-            let lat = points[1];
-            hppath.push(new BMapGL.Point(Number(lng), Number(lat)));
-        }
-
-        return (
-            <BaiduMap>
-                <BMapPrism
-                    points={lwpath}
-                    altitude={3000}
-                    topFillColor={'#5679ea'}
-                    topFillOpacity={0.6}
-                    sideFillColor={'#5679ea'}
-                    sideFillOpacity={0.9}
-                    enableMassClear={true}
-                    listeners={{ "click": this.handleBMapPrismClick }}
-                />
-                <BMapPrism
-                    points={hppath}
-                    altitude={3000}
-                    topFillColor={'#5679ea'}
-                    topFillOpacity={0.6}
-                    sideFillColor={'#5679ea'}
-                    sideFillOpacity={0.9}
-                    enableMassClear={true}
-                    listeners={{ "click": this.handleBMapPrismClick }}
-                />
-
-            </BaiduMap>
-        );
-    }
+		return (
+			<BaiduMap>
+				<BMapPrism
+					points={lwpath}
+					altitude={3000}
+					topFillColor={'#5679ea'}
+					topFillOpacity={0.6}
+					sideFillColor={'#5679ea'}
+					sideFillOpacity={0.9}
+					enableMassClear={true}
+					listeners={{ click: this.handleBMapPrismClick }}
+				/>
+				<BMapPrism
+					points={hppath}
+					altitude={3000}
+					topFillColor={'#5679ea'}
+					topFillOpacity={0.6}
+					sideFillColor={'#5679ea'}
+					sideFillOpacity={0.9}
+					enableMassClear={true}
+					listeners={{ click: this.handleBMapPrismClick }}
+				/>
+				<BMapTrackAnimation
+					poyline={pl}
+					overallView={true}
+					tilt={30}
+					duration={20000}
+					delay={3000}
+				/>
+			</BaiduMap>
+		);
+	}
 }
